7abff1568219b8202651ae4a3f4edee795f4973a
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-07-11 01:44:15 -0400

    bottom border now matches closer to the design

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 
 export default function NavBar() {
   return (
-    <nav className="border-slate-600 border-b-4 p-4">
+    <nav className="border-neutral-400 border-b-4 p-4">
         <span className="text-xl font-bold">Logo</span>
     </nav>
 
