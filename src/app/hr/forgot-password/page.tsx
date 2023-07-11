5d5c674da0a03bcd745c1f7bf091d12dcec951b5
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-07-11 01:54:31 -0400

    added a way to change the "company name logo" or whatever we do with it from the parent element

--- src/app/hr/forgot-password/page.tsx
+++ src/app/hr/forgot-password/page.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link'
 import NavBar from '../../../components/navbar'
 
 export default function forgotPassword() {
+    const companyName = "Real Company 2.0";
+
     return (
         <>
-        <NavBar/>
+        <NavBar logo={companyName}/>
         <main className="flex flex-col items-center pt-5">
-            <span className="text-2xl font-bold m-5">Logo</span>
+            <span className="text-2xl font-bold m-5">{companyName}</span>
             <span className="text-sm text-blue-500">Forgot Password</span>
             <span className="mt-10 font-semibold">Contact your administrator</span>
             <Link className="pt-5 italic text-s text-fuchsia-600" href="../../">&lt; back</Link>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,8 @@
 
-export default function NavBar() {
+export default function NavBar({logo}: {logo:string}) {
   return (
     <nav className="border-neutral-400 border-b-4 p-4">
-        <span className="text-xl font-bold">Logo</span>
+        <span className="text-xl font-bold">{logo}</span>
     </nav>
 
   )
