c08dd793f4ee131138cbdac486192012d966e372
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-07-11 01:34:13 -0400

    new navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+
+export default function NavBar() {
+  return (
+    <nav className="border-slate-600 border-b-4 p-4">
+        <span className="text-xl font-bold">Logo</span>
+    </nav>
+
+  )
+}
