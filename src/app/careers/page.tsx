abcab00d98859fb36e6448d6dc91a6a892cceb6a
Monica Tuttle <monica.tuttle16@gmail.com>
2023-07-12 07:21:24 -0400

    Changes to apply page

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image'
 
-export default function Home() {
+export default function Careers() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>JOBS</h1>
