98c635c774627f0b0825bcd71c6766fc2c6aa02e
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-07-12 03:01:11 -0400

    committing the fixed forgot password because this used the Nav bar but never got the fixed version committed

--- src/app/hr/forgot-password/page.tsx
+++ src/app/hr/forgot-password/page.tsx
@@ -1,17 +1,18 @@
 import Link from 'next/link'
-import NavBar from '../../../components/navbar'
+import NavBar from '@/components/navbar'
+import Logo from '@/components/Logo';
 
 export default function forgotPassword() {
     const companyName = "Real Company 2.0";
 
     return (
         <>
-        <NavBar logo={companyName}/>
+        <NavBar LeftItem={Logo}/>
         <main className="flex flex-col items-center pt-5">
             <span className="text-2xl font-bold m-5">{companyName}</span>
             <span className="text-sm text-blue-500">Forgot Password</span>
             <span className="mt-10 font-semibold">Contact your administrator</span>
-            <Link className="pt-5 italic text-s text-fuchsia-600" href="../../">&lt; back</Link>
+            <Link className="pt-5 italic text-s text-fuchsia-600" href="/hr">&lt; back</Link>
         </main>
         </>
     )
