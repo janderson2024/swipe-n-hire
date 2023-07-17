8e5958e4ba1cb278d9420cc8b0843b2cf0f7dd1f
Dennis Bowen <Dennisb723@gmail.com>
2023-07-16 19:26:20 -0500

    resume view updated and stylized

--- src/app/hr/[jobId]/resumes/page.tsx
+++ src/app/hr/[jobId]/resumes/page.tsx
@@ -48,11 +48,27 @@ export default function ViewResumes() {
           <h1 className="text-2xl block text-center font-semibold text-purple-700 mt-1">Position Title</h1>
         </div>
         <div>
-          <h2 className="text-l block text-center mt-4">1 of 20 Applications</h2>
+          <h2 className="text-l block text-center mt-2">1 of 20 Applications</h2>
+        </div>
+      </div>
+
+      <div className="flex justify-center mt-4">
+        <div className="flex items-center p-4">
+          <h2 className="text-l block text-center">Swipe to Reject</h2>
+        </div>
+        <div className="flex h-96 w-4/5 bg-gray-100 rounded-md">
+        </div>
+        <div className="flex items-center p-4">
+          <div>
+          <h1 className="text-2xl block text-center"></h1>
+          </div>
+          <h2 className="text-l block text-center">Swipe to Accept</h2>
         </div>
       </div>
         
-    
+      <div className="flex justify-center mt-4">
+        <h2 className="text-l block text-center font-semibold text-purple-700">View Applicant Information</h2>
+      </div>
 
     </main>
 
