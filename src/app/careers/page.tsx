1be3d7f18689635a84bc3018ced5295c64e7c668
Monica Tuttle <monica.tuttle16@gmail.com>
2023-08-05 22:30:39 -0400

    Changed code in filteredJobs

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -34,11 +34,9 @@ export default function JobPostings() {
       job.Job_Location,
       job.Job_Employment_Type,
     ];
-    return possibleFilters.some((possibleFilter) =>{
-      if(possibleFilter){
-        possibleFilter.includes(filter)
-      }
-    });
+    return possibleFilters.some((possibleFilter: any) =>
+      possibleFilter.includes(filter)
+    );
   });
 
   return (
@@ -57,14 +55,13 @@ export default function JobPostings() {
                 setFilter(event.target.value);
               }}
             />
-            <button className="bg-gray-200 p-2 rounded-r-md focus:outline-none">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6"
+                className="bg-gray-200 p-2 rounded-r-md focus:outline-none w-10 h-10"
                 onClick={toggleFilterButton}
               >
                 <path
@@ -73,14 +70,13 @@ export default function JobPostings() {
                   d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
                 />
               </svg>
-            </button>
           </div>
         </div>
         <div className="mt-8 border border-black rounded">
-          {filteredJobs.length === 0 ? (
-            <p className="p-4 text-center">No career openings</p>
-          ) : (
+          {filteredJobs.length > 0 ? (
             filteredJobs.map((job) => <JobPosting key={job.Job_ID} job={job} />)
+          ) : (
+            <p className="p-4 text-center">No openings</p>
           )}
         </div>
       </main>
