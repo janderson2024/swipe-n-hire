9d17782a93660599df2785fd5359c52b7662a370
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-08-08 23:28:50 -0400

    removed old "test job" stuff, and unused interfaces

--- src/types/job.tsx
+++ src/types/job.tsx
@@ -1,48 +1,3 @@
-export function getTestJobs() {
-  let testJobs: Job[] = [
-    {
-      JobID: 10,
-      JobTitle: "Real Job",
-      JobDate: "7/19/23",
-      JobEmployment: "Full-Time",
-      JobLocation: "City place USA",
-      JobDepartment: "Human Procurement",
-      JobCreator: "Person",
-      JobOpen: true,
-      JobOpenApplications: 40,
-      JobAcceptedApplications: 10,
-      JobRejectedApplications: 37,
-    },
-    {
-      JobID: 11,
-      JobTitle: "Fake Job",
-      JobDate: "7/29/23",
-      JobEmployment: "One Hour on Sunday",
-      JobLocation: "My living room",
-      JobDepartment: "Modeling",
-      JobCreator: "Alien",
-      JobOpen: false,
-      JobOpenApplications: 30,
-      JobAcceptedApplications: 2,
-      JobRejectedApplications: 124,
-    },
-    {
-      JobID: 12,
-      JobTitle: "CEO",
-      JobDate: "7/09/23",
-      JobEmployment: "Part-Time",
-      JobLocation: "New York",
-      JobDepartment: "Entire Company?",
-      JobCreator: "Worm",
-      JobOpen: true,
-      JobOpenApplications: 4,
-      JobAcceptedApplications: 12,
-      JobRejectedApplications: 2,
-    },
-  ];
-  return testJobs;
-}
-
 export interface JobDb {
   Job_ID: number;
   HR_Creator_ID?: number;
@@ -51,7 +6,7 @@ export interface JobDb {
   Job_Description?: string;
   Job_Date_Posted?: string;
 
-  Job_Reject_Email?: string;
+  Job_Rejected_Email?: string;
   Job_Accepted_Email?: string;
 
   Job_Salary?: string;
@@ -85,35 +40,7 @@ export interface EditJobDb extends JobDb {
   Accepted_Application_Count: number;
 }
 
-export interface Job {
-  JobID: number;
-
-  JobTitle: string;
-  JobDate?: string;
-  JobCreator?: string;
-  JobSalary?: string;
-  JobDepartment?: string;
-  JobEmployment?: string;
-  JobLocation?: string;
-
-  JobOpen: boolean;
-
-  JobOpenApplications: number;
-  JobAcceptedApplications: number;
-  JobRejectedApplications: number;
-}
-
-export interface ApplicantJobs {
-  JobID: number;
-  JobTitle: string;
-  JobDate?: string;
-  JobSalary?: string;
-  JobDepartment?: string;
-  JobEmployment?: string;
-  JobLocation?: string;
-}
-
-export interface Applications {
+export interface ApplicationDB {
   Job_ID: number;
   Application_ID?: number;
   Applicant_Name: string;
@@ -126,7 +53,7 @@ export interface Applications {
   Applicant_Resume?: string;
 }
 
-export interface ApplicationsForHR extends Applications {
+export interface ApplicationsForHR extends ApplicationDB {
   Applicant_Name: string;
   Applicant_Email: string;
   Applicant_Resume: string;
