0d96d1b1f3b0d7b4709f417b51e1491e0020c6ab
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-08-09 21:25:09 -0400

    updated email to use text instead of HTML since we aren't using html, and this adds line breaks

--- src/backend/sendEmail.tsx
+++ src/backend/sendEmail.tsx
@@ -6,14 +6,14 @@ const resend = new Resend(process.env['RESEND_API']);
 //example to use:
 //await sendEmail("josh@janderson2024.me", "Update about job: [job title]", "<b>Hello world!</b>");
 
-export default async function sendEmail(recipient:string, subject:string, htmlBody:string) {
+export default async function sendEmail(recipient:string, subject:string, emailBody:string) {
   console.log(`Sending email to ${recipient}`);
   try {
     const data = await resend.emails.send({
       from: 'Company Name <no-reply@swipe-n-hire.com>',
       to: [recipient],
       subject: subject,
-      html: htmlBody,
+      text: emailBody,
     });
     console.log("Email sent.")
   } catch (error) {
