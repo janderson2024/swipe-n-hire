ca2e8005859dddf040656f7b514f57039af86b44
Monica Tuttle <monica.tuttle16@gmail.com>
2023-08-13 21:16:16 -0400

    More changes to styling

--- src/components/BackToOpenings.tsx
+++ src/components/BackToOpenings.tsx
@@ -3,7 +3,7 @@ import BackArrow from "@/components/BackArrow";
 
 export default function BackToOpenings(backLink: string) {
     return (
-      <Link href={backLink} className="text-purple-700 flex items-center">
+      <Link href={backLink} className="text-white font-semibold flex items-center">
         <BackArrow/>
         Back to Openings
       </Link>
--- src/components/HRProfileImage.tsx
+++ src/components/HRProfileImage.tsx
@@ -22,7 +22,7 @@ export default function HRProfileImage() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth="2"
+      strokeWidth="1"
       stroke="currentColor"
       className="text-white rounded-full h-10 w-10"
     >
