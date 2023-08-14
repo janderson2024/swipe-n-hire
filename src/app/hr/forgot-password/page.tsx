be3ec44015f1bcf32647c51edef1947c7181e657
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-08-13 23:38:54 -0400

    added text-black to fix forgot-password

--- src/app/hr/forgot-password/page.tsx
+++ src/app/hr/forgot-password/page.tsx
@@ -9,7 +9,7 @@ export default function forgotPassword() {
       <NavBar LeftItem={<Logo />} />
       <main className="flex flex-col items-center pt-5">
         <div className="text-2xl font-bold m-5">
-          <Logo />
+          <Logo className="text-black"/>
         </div>
         <span className="text-sm text-blue-500">Forgot Password</span>
         <span className="mt-10 font-semibold">Contact your administrator</span>
