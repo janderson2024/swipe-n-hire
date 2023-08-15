5ba8ede0a792480b5267994e96e8a1e7e063685f
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-08-14 20:42:55 -0400

    reverting the  width

--- src/components/JobPosting.tsx
+++ src/components/JobPosting.tsx
@@ -8,7 +8,7 @@ export default function JobPosting({ job }: { job: JobDb }) {
       <div className="grid grid-cols-2 gap-4">
         <div className="col-span-2 md:col-span-1">
           <div className="text-s text-gray-4 m-4">ID: {job.Job_ID}</div>
-          <div className="w-1/4 pb-5">
+          <div className="pb-5">
             <Link className="text-purple-700 text-lg font-bold m-4 hover:text-purple-400" href={`/careers/${job.Job_ID}`}>{job.Job_Name}</Link>
           </div>
           <div className="text-gray-600 ml-4">Posted {job.Job_Date_Posted}</div>
