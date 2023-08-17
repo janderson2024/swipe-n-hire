832d40315919d9978f2dbc57c355068fcc4249ca
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-08-17 19:02:54 -0400

    2nd test version

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,8 +4,17 @@ export default function TestPage(){
 
     return (
         <>
-            <h1 className="hidden sm:block">Hi!</h1>
-            <h1 className="sm:hidden">Bye!</h1>
+            <h1 className="2xl:hidden">Shown Before 2XL</h1>
+            <h1 className="xl:hidden">Shown Before XL</h1>
+            <h1 className="lg:hidden">Shown Before Large</h1>
+            <h1 className="md:hidden">Shown Before Medium</h1>
+            <h1 className="sm:hidden">Shown Before Small</h1>
+            <h1 className="block"> Always Shown</h1>
+            <h1 className="sm:block hidden">Shown After Small</h1>
+            <h1 className="md:block hidden">Shown After Medium</h1>
+            <h1 className="lg:block hidden">Shown After Large</h1>
+            <h1 className="xl:block hidden">Shown After XL</h1>
+            <h1 className="2xl:block hidden">Shown After 2XL</h1>
         </>
 
     );
