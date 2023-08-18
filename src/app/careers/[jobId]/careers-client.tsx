99e6842281f93bae5632c875544f1ae25ef54a3f
Monica Tuttle <monica.tuttle16@gmail.com>
2023-08-17 20:38:55 -0400

    Working on mobile view for job description page

--- src/app/careers/[jobId]/careers-client.tsx
+++ src/app/careers/[jobId]/careers-client.tsx
@@ -20,11 +20,11 @@ function JobDescription({ job }: { job: JobDb }) {
 
   return (
     <>
-      <main className="flex flex-col items-center justify-center">
+      <main className="flex flex-col items-center bg-green-300 justify-center">
         <h1 className="text-lg font-bold mt-4">
           {job.Job_Name} - Job ID: {job.Job_ID}
         </h1>
-        <div className="w-full md:w-2/3 flex justify-between mb-4 px-6">
+        <div className="w-full md:w-2/3 bg-blue-400 flex justify-between mb-4 px-6 lg:flex flex-wrap">
           <div className="text-left">
             <p>
               <b>Department:</b> {job.Job_Department}
@@ -33,11 +33,11 @@ function JobDescription({ job }: { job: JobDb }) {
               <b>Employment Type:</b> {job.Job_Employment_Type}
             </p>
           </div>
-          <div className="text-right">
+          <div className="text-right bg-purple-500">
             <p>
               <b>Salary Range:</b> {job.Job_Salary}
             </p>
-            <p>
+            <p className="text-left">
               <b>Location:</b> {job.Job_Location}
             </p>
           </div>
@@ -52,15 +52,15 @@ function JobDescription({ job }: { job: JobDb }) {
           rows={16}
           className="resize-none mb-4 overflow-y-auto border rounded w-2/3 text-base px-2 py-1 border-purple-600 focus:outline-0"
         />
-        <div className="flex w-2/5 justify-evenly">
+        <div className="flex w-3/5 justify-evenly lg:flex flex-wrap">
           <Link className="mr-4" href={`/careers/${job.Job_ID}/apply`}>
-            <button className="bg-purple-700 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded">
+            <button className="bg-purple-700 hover:bg-purple-500 w-48 text-white font-bold py-2 px-4 rounded">
               Apply for this Job
             </button>
           </Link>
           <button
             onClick={openShareModal}
-            className="bg-purple-700 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded"
+            className="bg-purple-700 hover:bg-purple-500 w-48 text-white font-bold py-2 px-4 rounded"
           >
             Share this Job
           </button>
