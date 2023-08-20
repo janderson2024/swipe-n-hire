c0b4907ef4592d5cb51ab18af5628e2470339483
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-08-19 20:35:59 -0400

    GAHHH THE TAILWIND ISSUES IS BACK

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "@uploadthing/react/styles.css";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { getCompanyName } from "@/components/Logo";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
